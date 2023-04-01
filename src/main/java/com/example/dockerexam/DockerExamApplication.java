8778dd783ab6bbc21889a537a403c5e3e2a9b661
ap3334 <chwil2989@naver.com>
2023-04-01 20:17:58 +0900

    test

---
+++ src/main/java/com/example/dockerexam/DockerExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerExamApplication.class, args);
+    }
+
+}
